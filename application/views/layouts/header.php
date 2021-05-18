6c706e866822f6d225d981b29b4d24153f945b06
Your Name <mijail_2007_@hotmail.com>
2021-05-18 11:53:37 -0500

    add Footer & Header

--- application/views/layouts/header.php
+++ application/views/layouts/header.php
@@ -1,11 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-  <!--================================================================================
-	Item Name: Materialize - Material Design Admin Template
-	Version: 4.0
-	Author: PIXINVENT
-	Author URL: https://themeforest.net/user/pixinvent/portfolio
-  ================================================================================ -->
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -15,9 +9,9 @@
     <meta name="keywords" content="materialize, admin template, dashboard template, flat admin template, responsive admin template,">
     <title>Materialize - Material Design Admin Template</title>
     <!-- Favicons-->
-    <link rel="icon" href="<?=base_url()?>assets/admin/images/favicon/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" href="<?=base_url()?>assets/admin/images/favicon/A1.png" sizes="32x32">
     <!-- Favicons-->
-    <link rel="apple-touch-icon-precomposed" href="<?=base_url()?>assets/admin/images/favicon/apple-touch-icon-152x152.png">
+    <link rel="apple-touch-icon-precomposed" href="<?=base_url()?>assets/admin/images/favicon/logo1.png">
     <!-- For iPhone -->
     <meta name="msapplication-TileColor" content="#00bcd4">
     <meta name="msapplication-TileImage" content="images/favicon/mstile-144x144.png">
@@ -32,3 +26,237 @@
     <link href="<?=base_url()?>assets/admin/vendors/perfect-scrollbar/perfect-scrollbar.css" type="text/css" rel="stylesheet">
     <link href="<?=base_url()?>assets/admin/vendors/flag-icon/css/flag-icon.min.css" type="text/css" rel="stylesheet">
   </head>
+
+<!-- Start Page Loading -->
+<div id="loader-wrapper">
+      <div id="loader"></div>
+      <div class="loader-section section-left"></div>
+      <div class="loader-section section-right"></div>
+    </div>
+    <!-- End Page Loading -->
+
+
+      <!-- //////////////////////////////////////////////////////////////////////////// -->
+    <!-- START HEADER -->
+    <header id="header" class="page-topbar">
+      <!-- start header nav-->
+      <div class="navbar-fixed">
+        <nav class="navbar-color gradient-45deg-light-blue-cyan">
+          <div class="nav-wrapper">
+            <ul class="left">
+              <li>
+                <h1 class="logo-wrapper">
+                  <a href="index.html" class="brand-logo darken-1">
+                  <span class="logo-text hide-on-med-and-down">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span> 
+                    <img src="<?=base_url()?>assets/admin/images/logo/materialize-logo.png" alt="materialize logo">
+                    <!-- <span class="logo-text hide-on-med-and-down">UNICEF</span> -->
+                  </a>
+                </h1>
+              </li>
+            </ul>
+            <div class="header-search-wrapper hide-on-med-and-down">
+              <i class="material-icons">search</i>
+              <input type="text" name="Search" class="header-search-input z-depth-2" placeholder="Buscar " />
+            </div>
+            <ul class="right hide-on-med-and-down">
+              <li>
+                
+              </li>
+              <li>
+                <a href="javascript:void(0);" class="waves-effect waves-block waves-light toggle-fullscreen">
+                  <i class="material-icons">settings_overscan</i>
+                </a>
+              </li>
+              <li>
+                <a href="javascript:void(0);" class="waves-effect waves-block waves-light notification-button" data-activates="notifications-dropdown">
+                  <i class="material-icons">notifications_none
+                    <small class="notification-badge pink accent-2">5</small>
+                  </i>
+                </a>
+              </li>
+              <li>
+                <a href="javascript:void(0);" class="waves-effect waves-block waves-light profile-button" data-activates="profile-dropdown">
+                  <span class="avatar-status avatar-online">
+                    <img src="<?=base_url()?>assets/admin/images/avatar/avatar-7.png" alt="avatar">
+                    <i></i>
+                  </span>
+                </a>
+              </li>
+              <li>
+                <a href="#" data-activates="chat-out" class="waves-effect waves-block waves-light chat-collapse">
+                  <i class="material-icons">format_indent_increase</i>
+                </a>
+              </li>
+            </ul>
+           
+            <!-- notifications-dropdown -->
+            <ul id="notifications-dropdown" class="dropdown-content">
+              <li>
+                <h6>NOTIFICATIONS
+                  <span class="new badge">5</span>
+                </h6>
+              </li>
+              <li class="divider"></li>
+              <li>
+                <a href="#!" class="grey-text text-darken-2">
+                  <span class="material-icons icon-bg-circle cyan small">add_shopping_cart</span> A new order has been placed!</a>
+                <time class="media-meta" datetime="2015-06-12T20:50:48+08:00">2 hours ago</time>
+              </li>
+              <li>
+                <a href="#!" class="grey-text text-darken-2">
+                  <span class="material-icons icon-bg-circle red small">stars</span> Completed the task</a>
+                <time class="media-meta" datetime="2015-06-12T20:50:48+08:00">3 days ago</time>
+              </li>
+              <li>
+                <a href="#!" class="grey-text text-darken-2">
+                  <span class="material-icons icon-bg-circle teal small">settings</span> Settings updated</a>
+                <time class="media-meta" datetime="2015-06-12T20:50:48+08:00">4 days ago</time>
+              </li>
+              <li>
+                <a href="#!" class="grey-text text-darken-2">
+                  <span class="material-icons icon-bg-circle deep-orange small">today</span> Director meeting started</a>
+                <time class="media-meta" datetime="2015-06-12T20:50:48+08:00">6 days ago</time>
+              </li>
+              <li>
+                <a href="#!" class="grey-text text-darken-2">
+                  <span class="material-icons icon-bg-circle amber small">trending_up</span> Generate monthly report</a>
+                <time class="media-meta" datetime="2015-06-12T20:50:48+08:00">1 week ago</time>
+              </li>
+            </ul>
+            <!-- profile-dropdown -->
+            <ul id="profile-dropdown" class="dropdown-content">
+              <li>
+                <a href="#" class="grey-text text-darken-1">
+                  <i class="material-icons">face</i> Profile</a>
+              </li>
+              <li>
+                <a href="#" class="grey-text text-darken-1">
+                  <i class="material-icons">settings</i> Settings</a>
+              </li>
+              <li>
+                <a href="#" class="grey-text text-darken-1">
+                  <i class="material-icons">live_help</i> Help</a>
+              </li>
+              <li class="divider"></li>
+              <li>
+                <a href="#" class="grey-text text-darken-1">
+                  <i class="material-icons">lock_outline</i> Lock</a>
+              </li>
+              <li>
+                <a href="#" class="grey-text text-darken-1">
+                  <i class="material-icons">keyboard_tab</i> Logout</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+      <!-- end header nav-->
+    </header>
+    <!-- END HEADER -->
+    <!-- //////////////////////////////////////////////////////////////////////////// -->
+
+     <!-- START MAIN -->
+     <div id="main">
+      <!-- START WRAPPER -->
+      <div class="wrapper">
+        <!-- START LEFT SIDEBAR NAV-->
+        <aside id="left-sidebar-nav">
+          <ul id="slide-out" class="side-nav fixed leftside-navigation">
+            <li class="user-details light-blue ">
+              <div class="row">
+                <div class="col col s4 m4 l4">
+                  <img src="<?=base_url()?>assets/admin/images/avatar/logo.gif" alt="" class="circle responsive-img valign profile-image cyan" >
+                </div>
+                <div class="col col s8 m8 l8">
+                  <ul id="profile-dropdown-nav" class="dropdown-content">
+                    <li>
+                      <a href="#" class="grey-text text-darken-1">
+                        <i class="material-icons">face</i> Profile</a>
+                    </li>
+                    <li>
+                      <a href="#" class="grey-text text-darken-1">
+                        <i class="material-icons">settings</i> Settings</a>
+                    </li>
+                    <li>
+                      <a href="#" class="grey-text text-darken-1">
+                        <i class="material-icons">live_help</i> Help</a>
+                    </li>
+                    <li class="divider"></li>
+                    <li>
+                      <a href="#" class="grey-text text-darken-1">
+                        <i class="material-icons">lock_outline</i> Lock</a>
+                    </li>
+                    <li>
+                      <a href="#" class="grey-text text-darken-1">
+                        <i class="material-icons">keyboard_tab</i> Logout</a>
+                    </li>
+                  </ul>
+                  <a class="btn-flat dropdown-button waves-effect waves-light white-text profile-btn" href="#" data-activates="profile-dropdown-nav">UNICEF<i class="mdi-navigation-arrow-drop-down right"></i></a>
+                  <p class="user-roal">Administrador</p>
+                </div>
+              </div>
+            </li>
+            <li class="no-padding">
+              <ul class="collapsible" data-collapsible="accordion">
+                <li class="bold">
+                  <a href="index.html" class="waves-effect waves-cyan">
+                      <i class="material-icons">pie_chart_outlined</i>
+                      <span class="nav-text">Dashboard</span>
+                    </a>
+                </li>
+                <li class="bold">
+                  <a href="cards-basic.html" class="waves-effect waves-cyan">
+                      <i class="material-icons">cast</i>
+                      <span class="nav-text">CATEGORIA</span>
+                    </a>
+                </li>
+                <li class="bold">
+                  <a href="ui-basic-buttons.html" class="waves-effect waves-cyan">
+                      <i class="material-icons">insert_link</i>
+                      <span class="nav-text">Buttons</span>
+                    </a>
+                </li>
+                <li class="bold">
+                  <a href="form-layouts.html" class="waves-effect waves-cyan">
+                      <i class="material-icons">format_color_text</i>
+                      <span class="nav-text">Forms</span>
+                    </a>
+                </li>
+                <li class="bold">
+                  <a href="css-typography.html" class="waves-effect waves-cyan">
+                      <i class="material-icons">format_size</i>
+                      <span class="nav-text">Typography</span>
+                    </a>
+                </li>
+                <li class="bold">
+                  <a href="css-color.html" class="waves-effect waves-cyan">
+                      <i class="material-icons">invert_colors</i>
+                      <span class="nav-text">Color</span>
+                    </a>
+                </li>
+                <li class="bold">
+                  <a href="table-basic.html" class="waves-effect waves-cyan">
+                      <i class="material-icons">border_all</i>
+                      <span class="nav-text">Table</span>
+                    </a>
+                </li>
+                <li class="bold">
+                  <a href="ui-icons.html" class="waves-effect waves-cyan">
+                    <i class="material-icons">lightbulb_outline</i>
+                    <span class="nav-text">Icons</span>
+                  </a>
+                </li>
+                <li>
+                  <a class="btn waves-effect waves-light light-blue accent-4" href="https://pixinvent.com/materialize-material-design-admin-template/landing/" target="_blank">
+                    <i class="material-icons white-text">file_upload</i>Upgrade toe Pro!
+                  </a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+          <a href="#" data-activates="slide-out" class="sidebar-collapse btn-floating btn-medium waves-effect waves-light hide-on-large-only">
+            <i class="material-icons">menu</i>
+          </a>
+        </aside>
+        <!-- END LEFT SIDEBAR NAV-->
+        <!-- //////////////////////////////////////////////////////////////////////////// -->
